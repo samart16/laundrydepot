d914ebe42d8632372fd1d3b3f13e30864e358979
Steven Martinez <smartinez1092@gmail.com>
2023-04-10 17:08:12 -0500

    Alt Images

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,7 +9,7 @@ const CardItem = (props) => {
             <Link
             className='cards__item__link' to={props.path}>
                 <figure className='cards__item__pic-wrap' data-category={props.label}>
-                    <img src={props.src} className='cards__item__img'/>
+                    <img src={props.src} alt={props.alt} className='cards__item__img'/>
                 </figure>
                 <div className='cards__item__info'>
                     <h5 className='cards__item__text'>{props.text}</h5>
